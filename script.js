b48659e154f46d8e118141ff776f73fed978a976
Adnan Aheer <adnanaheer20@gmail.com>
2021-07-01 16:02:12 +0500

    created project

---
+++ script.js
@@ -0,0 +1,22 @@
+const toggles = document.querySelectorAll('.toogle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelectorAll('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(theClickOne) {
+    if (good.checked && cheap.checked && fast.checked) {
+        if (good === theClickOne) {
+            fast.checked = false
+        }
+
+        if(cheap === theClickOne) {
+            good.checked = false
+        }
+
+        if (fast === theClickOne) {
+            cheap.checked = false
+        }
+    }
+}
